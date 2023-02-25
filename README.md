c5abef772d141dffe8d736cad07a9105e22fffca
robo <robo2020@gmail.com>
2023-02-25 14:28:01 +0200

    switch to effort controller

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 
 ## To read
 - [Making a Mobile Robot #12 - ros2_control Concept & Simulation](https://articulatedrobotics.xyz/mobile-robot-12-ros2-control/)
+- [gazebo_ros2_control](https://github.com/ros-controls/gazebo_ros2_control)
 ## Exercise
 1. Convert URDF to support ros2_control
    1. use position control for both joints
@@ -17,12 +18,23 @@
 4. Extend the launch file control joint with position controller
    1. change slider and arm joint position from cli
 5. Change the slider joint to `effort` controller
-
+6. support xacro to switch between ros_control and gazebo plugin from launch/xacro argument
 
 ## Result
 
-### quiz 4
+### quiz 1
+[add control xacro file](src/urdf_demo/urdf/ros_control.urdf.xacro)
+### quiz 2
+[launch rviz and gazebo](src/urdf_demo/launch/lab4_v1.launch.py)
+### quiz 3
 `ros2 run controller_manager spawner joint_state_broadcaster`
 
+### quiz 4
 ```bash title="pub position command
 ros2 topic pub --once /position_controller/commands  std_msgs/msg/Float64MultiArray "{data: [10, 1.0]}"
+```
+
+### quiz 5
+```bash
+ros2 topic pub --once /effort_controller/commands  std_msgs/msg/Float64MultiArray "{data: [10]}"
+```
--- src/urdf_demo/launch/lab6_v2.launch.py
+++ src/urdf_demo/launch/lab6_v2.launch.py
@@ -3,9 +3,10 @@ import os
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration
 from launch.actions import (IncludeLaunchDescription,
-    RegisterEventHandler)
+    RegisterEventHandler,
+    DeclareLaunchArgument)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.event_handlers import OnProcessStart
 
@@ -18,9 +19,14 @@ def generate_launch_description():
     gazebo_pkg = get_package_share_directory("gazebo_ros")
     pkg = get_package_share_directory(PACKAGE)
 
+    use_effort = LaunchConfiguration('use_effort')
+    use_effort_arg =  DeclareLaunchArgument(
+            'use_effort',
+            default_value='true',
+            description='Use ros2_control if true')
 
     xacro_file = os.path.join(pkg, "urdf", URDF)
-    robot_description = Command(['xacro ', xacro_file])
+    robot_description = Command(['xacro ', xacro_file, ' use_effort:=', use_effort])
 
     robot_state_publisher = Node(
         package='robot_state_publisher',
@@ -54,7 +60,7 @@ def generate_launch_description():
         output="screen",
     )
 
-    controller_params_file = os.path.join(pkg,'config','position.yaml')
+    controller_params_file = os.path.join(pkg,'config','position_and_effort.yaml')
 
     controller_manager = Node(
         package="controller_manager",
@@ -75,15 +81,21 @@ def generate_launch_description():
         arguments=["position_controller"],
     )
 
+    effort_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["effort_controller"],
+    )
+
     delayed_controllers_spawner = RegisterEventHandler(
         event_handler=OnProcessStart(
             target_action=controller_manager,
-            on_start=[joint_broad_spawner, pos_controller_spawner],
+            on_start=[joint_broad_spawner, pos_controller_spawner, effort_controller_spawner],
         )
     )
 
     
-
+    ld.add_action(use_effort_arg)
     ld.add_action(robot_state_publisher)
     ld.add_action(gazebo)
     ld.add_action(spawn_entity)
